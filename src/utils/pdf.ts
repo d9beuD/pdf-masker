54a6306931c425392d171437f4df540ca2a02ed7
Vincent Bathelier <53569029+d9beuD@users.noreply.github.com>
2022-04-09 15:47:34 +0200

    Prise en compte des documents/pages cochées

--- src/utils/pdf.ts
+++ src/utils/pdf.ts
@@ -19,19 +19,29 @@ export async function getEditedPdf(
   // For each mask config we have
   for (let i = 0; i < masks.length; i += 1) {
     const mask = masks[i];
+    const pdfIndex = mask.documents.findIndex(
+      (doc) => doc.name === pdf.getTitle()
+    );
+    const pagesToApplyMaskTo = mask.documents[pdfIndex].pages;
 
-    pdf.getPages().forEach((page) => {
-      // Apply masks
-      page.drawRectangle(
-        isPreview
-          ? Object.assign({}, mask, {
-              borderColor: rgb(1, 0, 0),
-              borderWidth: 1,
-              opacity: 0.8,
-            } as mask)
-          : mask
-      );
-    });
+    // If PDF document is allowed to be masked
+    if (mask.documents[pdfIndex].applyMask) {
+      pdf.getPages().forEach((page, index) => {
+        // If PDF page is allowed to be masked
+        if (pagesToApplyMaskTo[index].applyMask) {
+          // Apply mask
+          page.drawRectangle(
+            isPreview
+              ? Object.assign({}, mask, {
+                  borderColor: rgb(1, 0, 0),
+                  borderWidth: 1,
+                  opacity: 0.8,
+                } as mask)
+              : mask
+          );
+        }
+      });
+    }
   }
 
   return pdf;
