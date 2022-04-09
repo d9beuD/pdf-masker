fee8f5d20f0802756636c93e47e7ae85e7172c2c
Vincent Bathelier <53569029+d9beuD@users.noreply.github.com>
2022-04-09 17:11:30 +0200

    Ajout des informations pour la compilation

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,21 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       nodeIntegration: true,
+      build: {
+        appId: "com.d9beud.pdf-masker",
+        mac: {
+          category: "public.app-category.utilities",
+          target: "dmg",
+          icon: "build/icons/256x256.icns",
+          darkModeSupport: true,
+          electronLanguage: ["en-US", "fr-FR"],
+        },
+        win: {
+          target: ["nsis", "portable"],
+          icon: "build/icons/icon.ico",
+          perMachine: true,
+        },
+      },
     },
   },
 };
