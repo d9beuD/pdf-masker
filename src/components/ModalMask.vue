a982c6ae95bd415a7550f16e02bb0232e76a95a3
Vincent Bathelier <53569029+d9beuD@users.noreply.github.com>
2022-05-20 18:05:57 +0200

    Suppression du contenu inutilisé

--- src/components/ModalMask.vue
+++ src/components/ModalMask.vue
@@ -13,53 +13,17 @@
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 import { BModal, BFormCheckbox } from "bootstrap-vue";
-import { configurable, documentToApplyMasksTo } from "@/types/mask";
+import { documentToApplyMasksTo } from "@/types/mask";
 import ModalDocumentSelector from "./ModalDocumentSelector.vue";
 
 @Component({ components: { BModal, BFormCheckbox, ModalDocumentSelector } })
-export default class PageSelectorModal extends Vue {
+export default class ModalMask extends Vue {
   @Prop({ type: String, required: true }) id!: string;
   @Prop({ type: Array, required: true }) documents!: documentToApplyMasksTo[];
   @Prop({ type: String, required: true }) maskName!: string;
 
   get title(): string {
     return "Sélection des pages - Masque " + this.maskName;
-  }
-
-  atLeastOneChecked(pages: configurable[]): boolean {
-    let atLeastOneChecked = false;
-
-    pages.forEach((page) => {
-      if (page.applyMask) {
-        atLeastOneChecked = true;
-      }
-    });
-
-    return atLeastOneChecked;
-  }
-
-  areAllChecked(pages: configurable[]): boolean {
-    let areAllChecked = true;
-
-    pages.forEach((page) => {
-      if (!page.applyMask) {
-        areAllChecked = false;
-      }
-    });
-
-    return areAllChecked;
-  }
-
-  areAllUnchecked(pages: configurable[]): boolean {
-    let areAllUnchecked = true;
-
-    pages.forEach((page) => {
-      if (page.applyMask) {
-        areAllUnchecked = false;
-      }
-    });
-
-    return areAllUnchecked;
   }
 }
 </script>
